0c5ace0c9112f313a46035b8dcf7ba2832970fb3
Dileep Varma Nero <79926748+Nero007x@users.noreply.github.com>
2025-06-15 03:38:39 +0530

    Time: 4 ms (51%), Space: 41.9 MB (42.68%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-    int m = s1.length(), n = s2.length();
-    if (m + n != s3.length()) return false;
-
-    boolean[][] dp = new boolean[m + 1][n + 1];
-    dp[0][0] = true;
-
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
-            if (i > 0)
-                dp[i][j] |= dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
-            if (j > 0)
-                dp[i][j] |= dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+        if(s3.length() != s1.length() + s2.length()){
+            return false;
         }
+
+        int n = s1.length(), m = s2.length();
+
+        boolean[][] dp = new boolean[n+1][m+1];
+        dp[0][0] = true; //interleaving of empty string is true
+
+        for(int i=0; i<= n; i++){
+            for(int j=0; j<=m; j++){
+                if(i>0)
+                    dp[i][j] |= (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+                if(j>0) 
+                    dp[i][j] |= (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1));
+            }
+        }
+        return dp[n][m];
     }
-
-    return dp[m][n];
-}
-
 }
