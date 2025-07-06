bb1f6b931826c2b68fdbd124f7740e61827a8fdd
Dileep Varma Nero <79926748+Nero007x@users.noreply.github.com>
2025-07-07 02:19:40 +0530

    Time: 56 ms (41.22%), Space: 54.2 MB (88.94%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -6,13 +6,14 @@ var compose = function(functions) {
     
     return function(x) {
         let result = x;
-        for(let i = functions.length-1; i>=0; i--){
+        for(let i = functions.length -1; i>= 0; i--){
             result = functions[i](result);
         }
         return result;
-        
     }
 };
+
+
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
