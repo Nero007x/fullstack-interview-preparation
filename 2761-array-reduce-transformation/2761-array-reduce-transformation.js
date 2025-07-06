9fdaeb039c492c932dfaa8bb3d30772f74d6fa89
Dileep Varma Nero <79926748+Nero007x@users.noreply.github.com>
2025-07-07 02:05:31 +0530

    Time: 47 ms (36.68%), Space: 55.7 MB (12.02%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -5,8 +5,8 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    result = init;
-    for(let i = 0; i<nums.length; i++){
+    let result = init;
+    for(let i = 0; i< nums.length; i++){
         result = fn(result, nums[i]);
     }
     return result;
