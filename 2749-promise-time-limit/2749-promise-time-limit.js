369cc6dfc72493de2f304cfc9ea0216fadc5fd77
Dileep Varma Nero <79926748+Nero007x@users.noreply.github.com>
2025-07-07 03:45:35 +0530

    Time: 47 ms (62.8%), Space: 54.4 MB (20.07%) - LeetHub

--- 2749-promise-time-limit/2749-promise-time-limit.js
+++ 2749-promise-time-limit/2749-promise-time-limit.js
@@ -6,14 +6,17 @@
 var timeLimit = function(fn, t) {
     
     return async function(...args) {
-        const timeoutPromise = new Promise(async (_, reject) =>{
+        const timeoutPromise = new Promise(async (resolve, reject) =>{
             setTimeout(() => reject("Time Limit Exceeded"), t);
+            try{
+                const res = await fn(...args);
+                resolve(res);
+            }catch(err){
+                reject(err);
+        }
         });
 
-        return await Promise.race([
-            fn(...args),
-            timeoutPromise
-        ]);
+        return timeoutPromise;
     }
 };
 
