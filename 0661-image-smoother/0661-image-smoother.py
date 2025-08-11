ae35313c0994e6a3bf9a22cee3bd2bbcb74bc3c2
Dileep Varma Nero <79926748+Nero007x@users.noreply.github.com>
2025-08-11 19:40:57 +0530

    Time: 142 ms (74.43%), Space: 18.8 MB (30.32%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,23 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        if not img or not img[0]:
+            return []
+
+        m, n = len(img), len(img[0])
+        result = [[0]*n for _ in range(m)]
+
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+        for i in range(m):
+            for j in range(n):
+
+                total = 0
+                count = 0
+
+                for di, dj in directions:
+                    ni, nj = i+di, j+dj
+                    if 0<=ni<m and 0 <=nj<n:
+                        total+=img[ni][nj]
+                        count+=1
+                result[i][j] = total//count
+        return result
