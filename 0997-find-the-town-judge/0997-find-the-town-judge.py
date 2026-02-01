40f00e5cc94772dbbe9d2dbd79990a121b3283c4
Dileep Varma Nero <79926748+Nero007x@users.noreply.github.com>
2026-02-01 17:54:56 +0530

    Time: 20 ms (41.1%), Space: 16 MB (69.18%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 class Solution(object):
     def findJudge(self, n, trust):
         """
@@ -7,19 +5,13 @@ class Solution(object):
         :type trust: List[List[int]]
         :rtype: int
         """
-        if n == 1 and not trust:
-            return 1
+        score = [0]*(n+1)
 
-        # Use a dictionary to store the score for each person ID
-        scores = defaultdict(int)
-
-        for a, b in trust:
-            scores[a] -= 1  # 'a' trusts someone -> decrement score
-            scores[b] += 1  # 'b' is trusted -> increment score
-
-        # Iterate 1 to n to see who meets the criteria
-        for i in range(1, n + 1):
-            if scores[i] == n - 1:
+        for t in trust:
+            score[t[0]] -=1
+            score[t[1]] +=1
+        
+        for i in range(1, n+1):
+            if score[i] ==n-1:
                 return i
-                
         return -1
